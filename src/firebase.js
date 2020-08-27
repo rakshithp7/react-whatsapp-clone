1c297a43cd95ea15bac3cbada1cf9a8f5515f613
Rakshith Poojary <rakshithp7@gmail.com>
2020-08-27 13:08:58 +0530

    Build base layout of whatsapp web app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCmU7R6v_aTOsLp5aYyJClrFhfKBpJ8doY",
+  authDomain: "whatsapp-clone-raks.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-raks.firebaseio.com",
+  projectId: "whatsapp-clone-raks",
+  storageBucket: "whatsapp-clone-raks.appspot.com",
+  messagingSenderId: "429998600720",
+  appId: "1:429998600720:web:c17220062b9ed7a2785095",
+  measurementId: "G-2B2YLMWYKN",
+};
